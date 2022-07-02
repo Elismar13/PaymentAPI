d7e843022366ff74eb126f9984aa57c2f8ed1781
Elismar13 <elismarsilva.5@outlook.com>
2022-07-02 17:25:53 -0300

    feat: 🎸 creating a query to find a wallet by name

--- src/main/java/br/com/payment/api/repository/WalletRepository.java
+++ src/main/java/br/com/payment/api/repository/WalletRepository.java
@@ -4,7 +4,12 @@ import br.com.payment.api.model.entity.wallet.Wallet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface WalletRepository extends CrudRepository<Wallet, String> {
+
+  Optional<Wallet> findByOwnerName(String ownerName);
 
 }
