d4b42fce07adf60c5afc29c4a412ea86bfe12732
Elismar13 <elismarsilva.5@outlook.com>
2022-07-03 15:01:55 -0300

    feat: :guitar: creating a query to get all payments made by a user on a day

--- src/main/java/br/com/payment/api/repository/PaymentRepository.java
+++ src/main/java/br/com/payment/api/repository/PaymentRepository.java
@@ -4,11 +4,15 @@ import br.com.payment.api.model.entity.payment.Payment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends CrudRepository<Payment, String> {
 
   Optional<Payment> findPaymentById(Integer id);
+
+  Optional<List<Payment>> findAllByOwnerIdAndCreationDateBetween(String ownerId, LocalDateTime startDate, LocalDateTime finalDate);
 
 }
