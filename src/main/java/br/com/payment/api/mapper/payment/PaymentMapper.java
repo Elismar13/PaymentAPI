2969372ab1b0b01d56e211cd84b209a77b822680
Elismar13 <elismarsilva.5@outlook.com>
2022-07-03 14:59:38 -0300

    feat: :guitar: mapping "date" field of PaymentDTO dto to "creationDate" of Payment

--- src/main/java/br/com/payment/api/mapper/payment/PaymentMapper.java
+++ src/main/java/br/com/payment/api/mapper/payment/PaymentMapper.java
@@ -4,6 +4,7 @@ import br.com.payment.api.model.dto.payment.request.PaymentDTO;
 import br.com.payment.api.model.dto.payment.response.PaymentResponseDTO;
 import br.com.payment.api.model.entity.payment.Payment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,6 +12,7 @@ public interface PaymentMapper {
 
   PaymentMapper INSTANCE = Mappers.getMapper(PaymentMapper.class);
 
+  @Mapping(source = "date", target = "creationDate")
   Payment toPayment(PaymentDTO PaymentDTO);
 
   PaymentResponseDTO toDTO(Payment Payment);
