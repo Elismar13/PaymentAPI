1c0146278b4aa4fd1541feb6ef52fc421f02f71d
Elismar13 <elismarsilva.5@outlook.com>
2022-07-04 16:25:46 -0300

    fix: 🐞 fixing username constants to accept whitespaces

--- src/main/java/br/com/payment/api/constants/RegexConstants.java
+++ src/main/java/br/com/payment/api/constants/RegexConstants.java
@@ -2,7 +2,7 @@ package br.com.payment.api.constants;
 
 public class RegexConstants {
 
-  public static final String REGEX_VALIDATE_LETTERS = "[a-zA-Z\\s]";
+  public static final String REGEX_VALIDATE_LETTERS = "[a-zA-Z ]+";
   public static final String REGEX_VALIDATE_UUID = "/^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$/gi";
 
 }
