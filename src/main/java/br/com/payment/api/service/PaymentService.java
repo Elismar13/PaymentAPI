829f6be5862a379226b282787aa2320a6910184e
Elismar13 <elismarsilva.5@outlook.com>
2022-07-04 13:34:14 -0300

    refactor: 💡 refactoring PaymentService responsibilities

--- src/main/java/br/com/payment/api/service/PaymentService.java
+++ src/main/java/br/com/payment/api/service/PaymentService.java
@@ -2,12 +2,16 @@ package br.com.payment.api.service;
 
 import br.com.payment.api.enums.PaymentIntervalsEnum;
 import br.com.payment.api.exception.payment.InsufficientPaymentLimitException;
+import br.com.payment.api.exception.wallet.WalletAlreadyExistsException;
+import br.com.payment.api.exception.wallet.WalletNotFountException;
 import br.com.payment.api.mapper.payment.PaymentMapper;
 import br.com.payment.api.model.dto.payment.request.PaymentDTO;
 import br.com.payment.api.model.dto.payment.response.PaymentResponseDTO;
 import br.com.payment.api.model.dto.payment.response.RemainingLimitResponseDTO;
 import br.com.payment.api.model.entity.payment.Payment;
+import br.com.payment.api.model.entity.wallet.Wallet;
 import br.com.payment.api.repository.PaymentRepository;
+import br.com.payment.api.repository.WalletRepository;
 import br.com.payment.api.utils.PaymentUtils;
 import org.javamoney.moneta.Money;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,56 +21,61 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+
+import static br.com.payment.api.utils.PaymentUtils.isMoneyOutOfLimit;
 
 @Service
 public class PaymentService {
 
+  private final PaymentMapper paymentMapper = PaymentMapper.INSTANCE;
   @Autowired
   private PaymentRepository paymentRepository;
+  @Autowired
+  private WalletRepository walletRepository;
 
-  private final PaymentMapper paymentMapper = PaymentMapper.INSTANCE;
+  public PaymentResponseDTO makePayment(UUID walletId, PaymentDTO paymentDTO, LocalDateTime dateTime) throws InsufficientPaymentLimitException, WalletAlreadyExistsException {
+    Optional<Wallet> wallet = walletRepository.findById(walletId);
 
-  public PaymentResponseDTO makePayment(String walletId, PaymentDTO paymentDTO, LocalDateTime dateTime) throws InsufficientPaymentLimitException {
-    paymentDTO.setOwnerId(walletId);
-    validatePayment(walletId, paymentDTO, dateTime);
-    Payment payment = paymentMapper.toPayment(paymentDTO);
-    Payment savedPayment = paymentRepository.save(payment);
-    return paymentMapper.toDTO(savedPayment);
+    if (wallet.isPresent()) {
+      Wallet savedWallet = wallet.get();
+      validatePayment(walletId, paymentDTO, dateTime);
+      Payment payment = paymentMapper.toPayment(paymentDTO);
+      payment.setWallet(savedWallet);
+      savedWallet.getPayments().add(payment);
+      walletRepository.save(savedWallet);
+      return paymentMapper.toDTO(payment);
+    }
+
+    throw new WalletNotFountException();
   }
 
-  public RemainingLimitResponseDTO checkRemainingLimit(String walletId, LocalDateTime dateTime) {
-    Money remainingLimit = getRemainingLimit(walletId, dateTime);
-    return RemainingLimitResponseDTO.builder()
-        .value(remainingLimit.getNumber())
-        .build();
-  }
-
-  private Money getTotalPaid(String walletId, LocalDateTime dateTime, Money amount) {
+  private Money getTotalPaid(UUID walletId, LocalDateTime dateTime, Money amount) {
     LocalDateTime firstDate = LocalDateTime.of(dateTime.toLocalDate(), LocalTime.of(0, 0));
     LocalDateTime lastDate = LocalDateTime.of(dateTime.toLocalDate(), LocalTime.of(23, 59, 59));
     Money zero = Money.of(0, "BRL");
 
     Optional<List<Payment>> dailyPayments = paymentRepository.findAllByWallet_IdAndCreationDateBetween(walletId, firstDate, lastDate);
 
-    if(dailyPayments.isPresent()) {
-       Optional<Money> totalSpent = dailyPayments.get().stream()
+    if (dailyPayments.isPresent()) {
+      Optional<Money> totalSpent = dailyPayments.get().stream()
           .map(payment -> Money.of(payment.getAmount(), "BRL"))
           .reduce(Money::add);
 
-       return totalSpent.orElse(zero).add(amount);
+      return totalSpent.orElse(zero).add(amount);
     } else {
       return zero;
     }
   }
 
-  private void isMoneyOutOfLimit(LocalDateTime dateTime, Money amount) throws InsufficientPaymentLimitException {
-    PaymentIntervalsEnum currentValidLimit = PaymentUtils.checkHourLimit(dateTime);
-    if(currentValidLimit.getAmount().subtract(amount).isNegative()) {
-      throw new InsufficientPaymentLimitException(amount.getNumber());
-    }
+  public RemainingLimitResponseDTO checkRemainingLimit(UUID walletId, LocalDateTime dateTime) {
+    Money remainingLimit = getRemainingLimit(walletId, dateTime);
+    return RemainingLimitResponseDTO.builder()
+        .value(remainingLimit.getNumber())
+        .build();
   }
 
-  private void validatePayment(String walletId, PaymentDTO paymentDTO, LocalDateTime dateTime) throws InsufficientPaymentLimitException {
+  private void validatePayment(UUID walletId, PaymentDTO paymentDTO, LocalDateTime dateTime) throws InsufficientPaymentLimitException {
     paymentDTO.setDate(dateTime);
     Money amount = Money.of(paymentDTO.getAmount(), "BRL");
 
@@ -75,7 +84,7 @@ public class PaymentService {
     isMoneyOutOfLimit(dateTime, totalPaid);
   }
 
-  private Money getRemainingLimit(String walletId, LocalDateTime dateTime) {
+  private Money getRemainingLimit(UUID walletId, LocalDateTime dateTime) {
     Money totalPaid = getTotalPaid(walletId, dateTime, Money.of(0, "BRL"));
     PaymentIntervalsEnum currentValidLimit = PaymentUtils.checkHourLimit(dateTime);
     return currentValidLimit.getAmount().subtract(totalPaid);
