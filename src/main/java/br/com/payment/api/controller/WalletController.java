32b1f433a0ffa074766557ca6008638c5a6b431a
Elismar13 <elismarsilva.5@outlook.com>
2022-07-04 13:35:38 -0300

    fix: 🐞 fixing request types on WalletController

--- src/main/java/br/com/payment/api/controller/WalletController.java
+++ src/main/java/br/com/payment/api/controller/WalletController.java
@@ -1,5 +1,6 @@
 package br.com.payment.api.controller;
 
+import br.com.payment.api.constants.RegexConstants;
 import br.com.payment.api.exception.wallet.WalletAlreadyExistsException;
 import br.com.payment.api.model.dto.payment.request.PaymentDTO;
 import br.com.payment.api.model.dto.payment.response.PaymentResponseDTO;
@@ -9,12 +10,16 @@ import br.com.payment.api.model.dto.wallet.response.WalletResponseDTO;
 import br.com.payment.api.service.PaymentService;
 import br.com.payment.api.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/wallets")
@@ -30,19 +35,23 @@ public class WalletController {
   public ResponseEntity<WalletResponseDTO> createNewUser(@RequestBody @Valid WalletDTO walletDTO) throws WalletAlreadyExistsException {
     WalletResponseDTO createdUser = walletService.createWallet(walletDTO);
 
-    return ResponseEntity.ok(createdUser);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
   }
 
 
   @GetMapping(value = "/{walletId}/limits", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<RemainingLimitResponseDTO> getAvailableCashLimit(@PathVariable("walletId") String walletId) {
+  public ResponseEntity<RemainingLimitResponseDTO> getAvailableCashLimit(@PathVariable("walletId") UUID walletId) {
     RemainingLimitResponseDTO checkRemainingLimit = paymentService.checkRemainingLimit(walletId, LocalDateTime.now());
 
-    return ResponseEntity.ok(checkRemainingLimit);
+    return ResponseEntity.status(HttpStatus.CREATED).body(checkRemainingLimit);
   }
 
   @PostMapping(value = "/{walletId}/payments", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<PaymentResponseDTO> makePayment(@PathVariable String walletId, @RequestBody @Valid PaymentDTO walletDTO) throws Exception {
+  public ResponseEntity<PaymentResponseDTO> makePayment(@PathVariable
+                                                          @NotNull
+                                                          @Pattern(regexp = RegexConstants.REGEX_VALIDATE_UUID, message = "{payment.wallet.uuid}")
+                                                              UUID walletId,
+                                                        @RequestBody @Valid PaymentDTO walletDTO) {
     PaymentResponseDTO createdPayment = paymentService.makePayment(walletId, walletDTO, LocalDateTime.now());
 
     return ResponseEntity.ok(createdPayment);
